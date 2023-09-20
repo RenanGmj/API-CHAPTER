a18c5b932c8e28c7f8b510e09da74af6eaedb79b
RenanGmj <renan.gmj87@gmail.com>
2023-09-20 11:21:41 -0300

    primeiro commit

---
+++ FS19_API_CHAPTER-main/Chapter/Chapter/Controllers/UsuarioController.cs
@@ -0,0 +1,97 @@
+﻿using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _IUsuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _IUsuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_IUsuarioRepository.Listar());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id) {
+            try
+            {
+                Usuario usuario = _IUsuarioRepository.BuscarPorId(id);
+
+                if (usuario == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(usuario);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            try
+            {
+                _IUsuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Usuario usuario)
+        {
+            try
+            {
+                _IUsuarioRepository.Atualizar(id, usuario);
+                return Ok("Usuario Alterado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _IUsuarioRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
